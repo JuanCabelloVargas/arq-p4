bad84c630e2c15233da3c69048dd1ed18601be37
JuanCabelloVargas <jicabello@miuandes.cl>
2025-11-10 15:10:27 -0300

    feat: generate asua assemblyu from rpn, user register a as accumulator, generate temporary variables. support mov, add and sub instructions

--- compiler.py
+++ compiler.py
@@ -2,9 +2,6 @@ VARIABLES = ["a", "b", "c", "d", "e", "f", "g"]
 
 
 def tokenize(line: str):
-    """
-    Convierte una línea en tokens
-    """
     tokens = []
     current = ""
     specials = set("+-=()")
@@ -29,7 +26,7 @@ def tokenize(line: str):
 
 
 
-def to_rpn(expr_tokens): #Shuting yard 
+def to_rpn(expr_tokens):
     output = []
     op_stack = []
     precedence = {"+": 1, "-": 1}
@@ -60,9 +57,69 @@ def to_rpn(expr_tokens): #Shuting yard
     return output
 
 
-def is_literal(token: str) -> bool: #check si es literal numerico o no, esto para data mas tarde
+def is_literal(token: str) -> bool: # check para literal numerico
     if not token:
         return False
     if token[0] in "+-":
         return token[1:].isdigit()
     return token.isdigit()
+
+
+def generate_asm_from_rpn(rpn): #lo generado en rpn lo pasamos a codigo asua
+    code = []
+    temps = []
+    stack = []
+    temp_id = 0
+
+    def new_temp():
+        nonlocal temp_id
+        temp_id += 1
+        name = f"t{temp_id}"
+        temps.append(name)
+        return name
+
+    for token in rpn:
+        if token in {"+", "-"}:
+            if len(stack) < 2:
+                raise ValueError("faltan operandos")
+            right = stack.pop()
+            left = stack.pop()
+            temp = new_temp()
+
+            # A = left
+            if is_literal(left):
+                code.append(f"    MOV A,{left}")
+            else:
+                code.append(f"    MOV A,(v_{left})")
+
+            # A = A ± right
+            if token == "+":
+                if is_literal(right):
+                    code.append(f"    ADD A,{right}")
+                else:
+                    code.append(f"    ADD A,(v_{right})")
+            else:
+                if is_literal(right):
+                    code.append(f"    SUB A,{right}")
+                else:
+                    code.append(f"    SUB A,(v_{right})")
+
+            # v_tN = A   (siempre en memoria)
+            code.append(f"    MOV (v_{temp}),A")
+
+            stack.append(temp)
+        else:
+            stack.append(token)
+
+    if len(stack) != 1:
+        raise ValueError("sobran operandos")
+
+    final = stack.pop()
+
+    if is_literal(final):
+        code.append(f"    MOV A,{final}")
+    else:
+        code.append(f"    MOV A,(v_{final})")
+    code.append("    MOV (v_result),A")
+
+    return code, temps
